9fd54e64bd08aba9eb116ca162d5cd2a3e1e6aa4
achatman <andrew.chatman9@gmail.com>
2018-11-08 22:06:40 -0600

    Added method signatures for WGraph

--- src/WGraph.java
+++ src/WGraph.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class WGraph {
+
+    public WGraph(String FName){}
+
+    public ArrayList<Integer> V2V(int ux, int uy, int vx, int vy){
+        return null;
+    }
+
+    public ArrayList<Integer> V2S(int ux, int uy, ArrayList<Integer> S){
+        return null;
+    }
+
+    public ArrayList<Integer> S2S(ArrayList<Integer> S1, ArrayList<Integer> S2){
+        return null;
+    }
+
+
 }
