4bcf4fd336d514db036daea64cb2ef598e62cdd6
achatman <andrew.chatman9@gmail.com>
2018-11-16 01:04:53 -0600

    Fixed reduce_by_one.

--- src/ImageProcessorTest.java
+++ src/ImageProcessorTest.java
@@ -24,9 +24,9 @@ public class ImageProcessorTest {
         ImageProcessor img = new ImageProcessor("src\\imgprocessor_ex.txt");
         img.reduce_by_one();
         ArrayList<ArrayList<Integer>> real = img.getImportance();
-        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(47351, 6048, 53797));
-        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(26381, 14067, 25958));
-        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(35444, 23271, 33355));
+        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(134604,6048,138480));
+        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(34365,14067,35743));
+        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(73813,23271,69303));
         ArrayList<ArrayList<Integer>> ideal = new ArrayList<>(Arrays.asList(row0, row1, row2));
         assertEquals(ideal, real);
     }
@@ -35,10 +35,11 @@ public class ImageProcessorTest {
     public void reduce_by_two(){
         ImageProcessor img = new ImageProcessor("src\\imgprocessor_ex.txt");
         img.reduce_by_one();
+        img.reduce_by_one();
         ArrayList<ArrayList<Integer>> real = img.getImportance();
-        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(47351, 53797));
-        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(26381, 25958));
-        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(35444, 33355));
+        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(8516,14962));
+        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(987,564));
+        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(14323,12234));
         ArrayList<ArrayList<Integer>> ideal = new ArrayList<>(Arrays.asList(row0, row1, row2));
         assertEquals(ideal, real);
     }
@@ -47,10 +48,12 @@ public class ImageProcessorTest {
     public void reduce_by_three(){
         ImageProcessor img = new ImageProcessor("src\\imgprocessor_ex.txt");
         img.reduce_by_one();
+        img.reduce_by_one();
+        img.reduce_by_one();
         ArrayList<ArrayList<Integer>> real = img.getImportance();
-        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(53797));
-        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(26381));
-        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(35444));
+        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(8516));
+        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(225));
+        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(11381));
         ArrayList<ArrayList<Integer>> ideal = new ArrayList<>(Arrays.asList(row0, row1, row2));
         assertEquals(ideal, real);
     }
