c307daad319287546f8e328613fe8f3b3acf5f75
achatman <andrew.chatman9@gmail.com>
2018-11-15 22:21:44 -0600

    Added testcase.

--- src/ImageProcessorTest.java
+++ src/ImageProcessorTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +10,13 @@ public class ImageProcessorTest {
 
     @Test
     public void getImportance() {
-        ImageProcessor img = new ImageProcessor("imgprocessor_ex2.txt");
-        img.getImportance();
+        ImageProcessor img = new ImageProcessor("imgprocessor_ex.txt");
+        ArrayList<ArrayList<Integer>> real = img.getImportance();
+        ArrayList<Integer> row0 = new ArrayList<>(Arrays.asList(8981, 47351, 6048, 53797));
+        ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(20545, 26381, 14067, 25958));
+        ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(11408, 35444, 23271, 33355));
+        ArrayList<ArrayList<Integer>> ideal = new ArrayList<>(Arrays.asList(row0, row1, row2));
+        assertEquals(ideal, real);
     }
 
 //    @Test
